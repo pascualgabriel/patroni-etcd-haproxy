8f61a8d28a359352129544a3f3643b1f6aca1a47
Gabriel Pascual <gabrielfpascual@gmail.com>
2021-10-05 23:13:51 -0300

    Using enable command to start services

--- HAProxy/haproxy-02-bootstrap.sh
+++ HAProxy/haproxy-02-bootstrap.sh
@@ -7,12 +7,10 @@ dnf install -y keepalived haproxy psmisc
 sudo mv -v /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg_sample \
     && cp -v /vagrant/config/haproxy.cfg /etc/haproxy/haproxy.cfg \
     && setsebool -P haproxy_connect_any=1 \
-    && systemctl enable haproxy \
-    && systemctl start haproxy
+    && systemctl enable --now haproxy
 
 # KeepAlived
 
 sudo mv -v /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf_sample \
     && cp -v /vagrant/config/keepalived-02.conf /etc/keepalived/keepalived.conf \
-    && systemctl enable keepalived \
-    && systemctl start keepalived
+    && systemctl enable --now keepalived
