975fc9ff9d705e241369136483caa461917008b4
Gabriel Pascual <gabrielfpascual@gmail.com>
2022-02-17 23:08:07 -0300

    Ordering and refactoring code

--- barman/vm/bootstrap.sh
+++ barman/vm/bootstrap.sh
@@ -2,13 +2,13 @@
 
 WORKDIR=$(dirname $0)
 
-echo "barman:${OS_BARMAN_USER_PASS}" | chpasswd # Get global env
-
 cd "$WORKDIR"
 
 su root -c './prepare-env.sh'
 
 su barman -c './config-barman.sh'
 
+echo "barman:${OS_BARMAN_USER_PASS}" | chpasswd # Get global env
+
 echo "export OS_PATRONI_USER_PASS=$OS_PATRONI_USER_PASS" >> ~barman/.bash_profile \
-&& chown barman.barman ~barman/.bash_profile
+    && chown barman.barman ~barman/.bash_profile
